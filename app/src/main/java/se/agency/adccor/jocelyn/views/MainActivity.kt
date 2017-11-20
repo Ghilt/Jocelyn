b98df60fc83e3b4053398e84a1cc94aaf4a7d905
Adam Nilsson <adam.n.a3@gmail.com>
2017-11-20 19:25:02 +0100

    Initial Android structure

---
+++ app/src/main/java/se/agency/adccor/jocelyn/views/MainActivity.kt
@@ -0,0 +1,18 @@
+package se.agency.adccor.jocelyn.views
+
+import android.app.Activity
+import android.os.Bundle
+import se.agency.adccor.jocelyn.R
+
+/**
+ * Created by adamn on 11/20/2017.
+ */
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
---
+++ app/src/main/java/se/agency/adccor/jocelyn/views/SplashActivity.kt
@@ -0,0 +1,15 @@
+package se.agency.adccor.jocelyn.views
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+
+class SplashActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
