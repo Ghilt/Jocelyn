bc81f462c2abc6497231c9559a6ce2ea048d6490
Adam Nilsson <adam.n.a3@gmail.com>
2017-11-26 18:10:01 +0100

    Add test airplane send animation (careful: developer-UI-stylish ATM)

--- app/src/main/java/se/agency/adccor/jocelyn/views/MainActivity.kt
+++ app/src/main/java/se/agency/adccor/jocelyn/views/MainActivity.kt
@@ -1,8 +1,11 @@
 package se.agency.adccor.jocelyn.views
 
 import android.app.Activity
+import android.graphics.drawable.Animatable
 import android.os.Bundle
+import android.util.Log
 import com.sothree.slidinguppanel.SlidingUpPanelLayout
+import kotlinx.android.synthetic.main.chat_input_layout.*
 import se.agency.adccor.jocelyn.R
 import se.agency.adccor.jocelyn.views.dummy.DummyContent
 import se.agency.adccor.jocelyn.views.listeners.ChatFragmentSlideListener
@@ -20,6 +23,14 @@ class MainActivity : Activity(), ChatFragment.OnListFragmentInteractionListener 
         val slidingLayout: SlidingUpPanelLayout = findViewById(R.id.sliding_layout)
         val chatFragment = fragmentManager.getChatFragment()
         slidingLayout.addPanelSlideListener(ChatFragmentSlideListener(this, chatFragment))
+
+        buttonSend.setOnClickListener {
+            Log.d("spx ", "   af af ")
+            val plane = it.background
+            if(plane is Animatable){
+                plane.start()
+            }
+        }
     }
 
     override fun onListFragmentInteraction(item: DummyContent.DummyItem) {
